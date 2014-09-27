fe4e439800f4b882a64280f81bc37711f5e56095
ivarsju <ivars.ju@gmail.com>
2014-09-27 13:14:56 +0300

    removed AUTO_INCREMENT from migration

--- migrations/m140917_173601_init_2.php
+++ migrations/m140917_173601_init_2.php
@@ -20,14 +20,14 @@ class m140917_173601_init_2 extends CDbMigration
               `pcnt_id` tinyint(3) unsigned NOT NULL AUTO_INCREMENT,
               `pcnt_name` char(40) NOT NULL COMMENT 'Contact type',
               PRIMARY KEY (`pcnt_id`)
-            ) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
+            ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
             CREATE TABLE `pdcm_document_type` (
               `pdcm_id` tinyint(3) unsigned NOT NULL AUTO_INCREMENT,
               `pdcm_name` varchar(50) NOT NULL COMMENT 'Document type',
               `pdcm_hidded` tinyint(4) NOT NULL DEFAULT '0',
               PRIMARY KEY (`pdcm_id`)
-            ) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8;
+            ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
             CREATE TABLE `ppcn_person_contact` (
               `ppcn_id` smallint(5) unsigned NOT NULL AUTO_INCREMENT,
@@ -41,14 +41,14 @@ class m140917_173601_init_2 extends CDbMigration
               KEY `ppcn_pcnt_type` (`ppcn_pcnt_type`),
               CONSTRAINT `ppcn_person_contact_ibfk_1` FOREIGN KEY (`ppcn_pprs_id`) REFERENCES `pprs_person` (`pprs_id`),
               CONSTRAINT `ppcn_person_contact_ibfk_2` FOREIGN KEY (`ppcn_pcnt_type`) REFERENCES `pcnt_contact_type` (`pcnt_id`)
-            ) ENGINE=InnoDB AUTO_INCREMENT=33 DEFAULT CHARSET=utf8;
+            ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
             CREATE TABLE `pprs_person` (
               `pprs_id` smallint(5) unsigned NOT NULL AUTO_INCREMENT,
               `pprs_first_name` varchar(100) CHARACTER SET utf8 DEFAULT NULL COMMENT 'First name',
               `pprs_second_name` varchar(100) CHARACTER SET utf8 DEFAULT NULL COMMENT 'Last name',
               PRIMARY KEY (`pprs_id`)
-            ) ENGINE=InnoDB AUTO_INCREMENT=77 DEFAULT CHARSET=latin1;
+            ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
             CREATE TABLE `ppxd_person_x_document` (
               `ppxd_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
@@ -64,7 +64,7 @@ class m140917_173601_init_2 extends CDbMigration
               KEY `ppxd_pdcm_id` (`ppxd_pdcm_id`),
               CONSTRAINT `ppxd_person_x_document_ibfk_1` FOREIGN KEY (`ppxd_pprs_id`) REFERENCES `pprs_person` (`pprs_id`),
               CONSTRAINT `ppxd_person_x_document_ibfk_2` FOREIGN KEY (`ppxd_pdcm_id`) REFERENCES `pdcm_document_type` (`pdcm_id`)
-            ) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8;
+            ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
             CREATE TABLE `ppxs_person_x_setting` (
               `ppxs_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
@@ -78,7 +78,7 @@ class m140917_173601_init_2 extends CDbMigration
               KEY `constrain_ppxs_psty` (`ppxs_psty_id`),
               CONSTRAINT `constrain_ppxs_pprs` FOREIGN KEY (`ppxs_pprs_id`) REFERENCES `pprs_person` (`pprs_id`),
               CONSTRAINT `constrain_ppxs_psty` FOREIGN KEY (`ppxs_psty_id`) REFERENCES `psty_setting_type` (`psty_id`)
-            ) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
+            ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 
             CREATE TABLE `ppxt_person_x_type` (
@@ -90,7 +90,7 @@ class m140917_173601_init_2 extends CDbMigration
               KEY `ppxt_pprs_id` (`ppxt_pprs_id`),
               CONSTRAINT `ppxt_person_x_type_ibfk_1` FOREIGN KEY (`ppxt_ptyp_id`) REFERENCES `ptyp_type` (`ptyp_id`),
               CONSTRAINT `ppxt_person_x_type_ibfk_2` FOREIGN KEY (`ppxt_pprs_id`) REFERENCES `pprs_person` (`pprs_id`)
-            ) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=latin1;
+            ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 
             CREATE TABLE `psty_setting_type` (
@@ -99,7 +99,7 @@ class m140917_173601_init_2 extends CDbMigration
               `psty_field_type` enum('int','char','date','email','money') NOT NULL,
               `psty_hidded` tinyint(4) NOT NULL DEFAULT '0',
               PRIMARY KEY (`psty_id`)
-            ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+            ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 
             CREATE TABLE `ptyp_type` (
@@ -107,7 +107,7 @@ class m140917_173601_init_2 extends CDbMigration
               `ptyp_name` char(50) NOT NULL COMMENT 'Group',
               `ptyp_hidden` tinyint(3) unsigned NOT NULL DEFAULT '0' COMMENT 'Status',
               PRIMARY KEY (`ptyp_id`)
-            ) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+            ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
             SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
             
